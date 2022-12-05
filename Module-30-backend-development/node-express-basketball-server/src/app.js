533a2bc0ac498308b9327596759288cf1b266ec1
sdahal1 <raabdahl@gmail.com>
2022-12-05 11:43:17 -0800

    week 1 node monday pm

--- Module-30-backend-development/node-express-basketball-server/src/app.js
+++ Module-30-backend-development/node-express-basketball-server/src/app.js
@@ -14,11 +14,15 @@ const teams = [
     },
     {
         name: "Hawks",
-        city: "Atlana"
+        city: "Atlanta"
     },
     {
         name: "Celtics",
         city: "Boston"
+    },
+    {
+        name: "Falcons",
+        city: "Atlanta"
     }
 ]
 
@@ -37,20 +41,86 @@ const middlewareCheckAccountCredentials = (req,res,next)=>{
 
 /* response function that will give the final response to the client */
 const sayHello =  (req,res,next)=>{
+    const {name} = req.query;
+
+    let message = name? `Hello ${name}, welcome to the basketball server!` : "Hello User, welcome to the basketball server!"
     // res.json({message: "Bank balance is: 10000"})
-    res.send("Hello User, welcome to the basketball server!");
+    res.send(message);
 }
 
 
 const getAllTeams = (req,res,next)=>{
-    res.json({data: teams})
+    // console.log(req.query)
+    const {city} = req.query;
+
+    //if query param for city is given, then only give back teams from that city
+    if(city !== undefined){
+        let filteredResult = teams.filter((team)=>team.city === city);
+        if(filteredResult.length === 0){
+            //if you call next() with content inside the (), then the next() will trigger the error handler function
+            next("Team from this city is not available...try another city");
+            // next();
+        }
+        res.json({data: filteredResult})
+    }else{
+        //otherwise give all the teams
+        res.json({data: teams})
+    }
 }
 
 //app.use just tells express app which functions to use in what order. You will want the function that returns a response to the client to be the latter of the two
 app.use(morgan("dev"));
 app.use(middlewareCheckAccountCredentials);
+
+
+
+
+
+/* Routes */
 app.get("/", sayHello)
+
 app.get("/teams",getAllTeams)
+
+app.get("/teams/:id",(req,res,next)=>{
+    const {id} = req.params;
+    if(id >= teams.length){
+        next(`Team with the id of: ${id} does not exist. `)
+    }
+    res.json({data: teams[id]})
+})
+
+app.get("/say/goodbye", (req, res, next) => {
+    
+    const name = req.query.name; //get name from req.query ( ?variable)
+
+    const content = name ? `Peace out, ${name}!` : `Peace be with you fam.`;
+    res.send(content);
+})
+
+app.get("/say/:greeting", (req, res, next) => {
+    const greeting = req.params.greeting; //get greeting from req.params (:variable)
+    const name = req.query.name; //get name from req.query ( ?variable)
+
+    const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+    res.send(content);
+})
+
+
+// Not-found handler- have this AFTER all of your routes
+app.use((req, res, next) => {
+    //code to make morgan freeman audio play some friendly message would go here
+    res.send(`The route ${req.path} does not exist!`);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.log("error handler has been triggered!", err);
+    res.send(err);
+  });
+
+
+
+
 
 // app.get("/teams",(req,res,next)=>{
 //     res.json(teams)
